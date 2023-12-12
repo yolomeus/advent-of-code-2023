09db7b55ae714d550658663163225c8eb7b07f0a
Fabian Beringer <fabian.beringer@smartsteuer.de>
2023-12-12 22:21:18 +0100

    faster (but kinda ugly) solution for day 5 part 2

--- day_05.py
+++ day_05.py
@@ -1,12 +1,9 @@
 from functools import partial
-from multiprocessing import Pool
-
-from tqdm import tqdm
 
 from utils import read_file
 
 
-def parse_mapping(mapping_str):
+def parse_mapping(mapping_str: str):
     dest, src, length = map(int, mapping_str.split())
     return {'src_range': range(src, src + length), 'dest': dest}
 
@@ -17,7 +14,7 @@ def parse_map(map_str: str):
     return mappings
 
 
-def parse_data(data):
+def parse_data(data: str):
     data = data.strip().split('\n\n')
     seeds, maps = data[0], data[1:]
 
@@ -32,13 +29,65 @@ def parse_data(data):
 def get_location_number(seed, maps):
     next_value = seed
     for map_ in maps:
-        for mappings in map_:
-            src_range = mappings['src_range']
+        for map_item in map_:
+            src_range = map_item['src_range']
             if next_value in src_range:
-                next_value = mappings['dest'] + next_value - src_range[0]
+                next_value = map_item['dest'] + next_value - src_range[0]
                 break
 
     return next_value
+
+
+def shift_num(num: int, src_range: range, dest: int):
+    return num - src_range.start + dest
+
+
+def shift_range(range_: range, src_range: range, dest: int):
+    return range(shift_num(range_.start, src_range, dest),
+                 shift_num(range_.stop, src_range, dest))
+
+
+def split_range(map_: list[dict[str, range]], range_: range):
+    ranges = []
+    new_range_queue = [range_]
+
+    while len(new_range_queue) > 0:
+        new_range = new_range_queue.pop(0)
+        found_match = False
+        for map_item in map_:
+            src_range, dest = map_item['src_range'], map_item['dest']
+            if new_range.start in src_range:
+                found_match = True
+                if new_range.stop > src_range.stop:
+                    ranges.append(range(new_range.start, src_range.stop))
+                    new_range_queue.append(range(src_range.stop, new_range.stop))
+                else:
+                    ranges.append(new_range)
+
+        if not found_match:
+            ranges.append(new_range)
+
+    return ranges
+
+
+def map_range(range_: range, map_: list[dict[str, range]]) -> list[range]:
+    mapped_ranges = []
+    # split range based on maps if too large
+    sub_ranges = split_range(map_, range_)
+    for sub_range in sub_ranges:
+        shifted = False
+        for mapping in map_:
+            src_range, dest = mapping['src_range'], mapping['dest']
+            if sub_range.start in src_range:
+                mapped_ranges.append(shift_range(sub_range, src_range, dest))
+                shifted = True
+                break
+
+        # any range that we did not shift maps to itself
+        if not shifted:
+            mapped_ranges.append(sub_range)
+
+    return mapped_ranges
 
 
 def main():
@@ -46,20 +95,16 @@ def main():
     seeds, maps = parse_data(data)
 
     location_numbers = list(map(partial(get_location_number, maps=maps), seeds))
-    print(min(location_numbers))
+    print('part 1 :', min(location_numbers))
 
-    # brute force for the giggles (takes 15min on my machine...)
-    seeds = (range(x, x + y) for x, y in zip(seeds[0::2], seeds[1::2]))
-    seeds = tuple(y for x in tqdm(seeds) for y in x)
+    ranges = (range(x, x + y) for x, y in zip(seeds[0::2], seeds[1::2]))
+    for map_ in maps:
+        ranges = [map_range(range_, map_) for range_ in ranges]
+        # flatten
+        ranges = [y for x in ranges for y in x]
 
-    with Pool(12) as p:
-        location_numbers = (num for num in
-                            tqdm(p.imap_unordered(partial(get_location_number, maps=maps),
-                                                  seeds,
-                                                  chunksize=4096),
-                                 total=len(seeds))
-                            )
-        print(min(location_numbers))
+    # we can easily get the min of a range by selecting its lower bound
+    print('part 2 :', min(range_.start for range_ in ranges))
 
 
 if __name__ == '__main__':
